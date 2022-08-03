3a0200d90dfa5fa05c934ebb0abc86016dcfcf49
Aline Soliman <alineapreis@hotmail.com>
2022-08-03 18:12:03 -0300

    feat: inclui função para chamar o arquivo talker para eitura de palestrantes e exibição de status de chamada

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const talkers = require('./speaker.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,11 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_request, response) => {
+  const orator = await talkers();
+  response.status(HTTP_OK_STATUS).send(orator);
 });
 
 app.listen(PORT, () => {
